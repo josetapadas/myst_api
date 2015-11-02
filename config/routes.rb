855072859625afbe6b893f80c7e880b4d12074c8
Jose Alves <j.alves@runtime-revolution.com>
2015-11-02 16:09:26 +0000

    ma-1: created simple versioned route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :api, defaults: { format: json }, path: '/' do
+    scope module: :V1 do
+
+    end
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
