629335e35d6c199509e2a05b41c49a23c3a400b6
Jose Alves <j.alves@runtime-revolution.com>
2015-11-07 14:38:37 +0000

    ma-4: added User#create action

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,6 +5,21 @@ module Api
         @user = User.find(params[:id])
         render json: @user, serializer: UserSerializer
       end
+
+      def create
+        @user = User.new(user_params)
+        if @user.save
+          render json: @user, status: 201, location: [:api, @user]
+        else
+          render json: { errors: @user.errors }, status: 422
+        end
+      end
+
+      private
+
+      def user_params
+        params.require(:user).permit(:email, :password, :password_confirmation)
+      end
     end
   end
 end
--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -16,4 +16,30 @@ describe Api::V1::UsersController, type: :controller do
       expect(reply[:user][:email]).to eql(@user.email)
     end
   end
+
+  describe "#create" do
+    context "successfully creating users" do
+      before(:each) do
+        @user_params = FactoryGirl.attributes_for(:user)
+        post(:create, { user: @user_params }, format: :json)
+      end
+
+        it "should return the created user" do
+          reply = JSON.parse(response.body, symbolize_names: true)
+          expect(reply[:user][:email]).to(eql @user_params[:email])
+      end
+    end
+
+    context "errors while creating users" do
+      before(:each) do
+        missing_params = { password: "le_password", password_confirmation: "le_password" }
+        post(:create, { user: missing_params }, format: :json)
+      end
+
+      it "should return an erroneus message" do
+        reply = JSON.parse(response.body, symbolize_names: true)
+        expect(reply[:errors][:email]).to include("can't be blank")
+      end
+    end
+  end
 end
