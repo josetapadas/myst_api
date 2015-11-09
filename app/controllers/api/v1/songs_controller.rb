c7d4a10851e82d74ff52fbc0f8c124cbc836f4e6
Jose Alves <j.alves@runtime-revolution.com>
2015-11-09 20:23:00 +0000

    ma-5: added Song ownership

--- app/controllers/api/v1/songs_controller.rb
+++ app/controllers/api/v1/songs_controller.rb
@@ -1,7 +1,7 @@
 module Api
   module V1
     class SongsController < ApplicationController
-      before_action :authenticate!, only: [:create]
+      before_action :authenticate!, only: [:create, :update, :destroy]
 
       def index
         @songs = Song.all
@@ -24,11 +24,20 @@ module Api
       end
 
       def destroy
-        user_songs = current_user.songs.find(params[:id])
-        if user_songs.destroy
+        user_song = current_user.songs.find(params[:id])
+        if user_song.destroy
           head 200
         else
           head 422
+        end
+      end
+
+      def update
+        current_song = current_user.songs.find(params[:id])
+        if current_song.update(song_params)
+          render json: current_song, status: 200, location: [:api, current_song]
+        else
+          render json: { errors: current_song.errors }, status: 422
         end
       end
 
--- spec/controllers/api/v1/songs_controller_spec.rb
+++ spec/controllers/api/v1/songs_controller_spec.rb
@@ -19,4 +19,60 @@ describe Api::V1::SongsController, type: :controller do
       end
     end
   end
+
+  describe "#update" do
+    before(:each) do
+      @user = FactoryGirl.create(:user)
+      @song = FactoryGirl.create(:song, user: @user)
+      request.headers['Authorization'] =  @user.authentication_token
+    end
+
+    it "should update the song successfully" do
+      patch(:update, { user_id: @user.id, id: @song.id, song: { title: "Echoes" } })
+      reply = JSON.parse(response.body, symbolize_names: true)
+      expect(reply[:song][:title]).to eq("Echoes")
+      expect(response.status).to eq(200)
+    end
+
+    it "should not update a song with invalid parameters" do
+      patch(:update, { user_id: @user.id, id: @song.id, song: { title: "" } })
+      reply = JSON.parse(response.body, symbolize_names: true)
+
+      expect(reply[:errors][:title]).to include("can't be blank")
+      expect(response.status).to eq(422)
+    end
+
+    it "should not allow another user to update a song" do
+      request.headers['Authorization'] =  "lollertoken"
+
+      patch(:update, { user_id: '10', id: @song.id, song: { title: "Never gonna give you up" } })
+      reply = JSON.parse(response.body, symbolize_names: true)
+
+      expect(reply[:errors]).to be_eql("Not authenticated")
+      expect(response.status).to eq(401)
+    end
+
+    describe "#destroy" do
+      before(:each) do
+        @user = FactoryGirl.create(:user)
+        @song = FactoryGirl.create(:song, user: @user)
+        request.headers['Authorization'] =  @user.authentication_token
+      end
+
+      it "should allow a user to delete a song" do
+        delete(:destroy, { user_id: @user.id, id: @song.id })
+      end
+
+      it "should not allow another user to delete a song" do
+        request.headers['Authorization'] =  "lollertoken"
+
+        delete(:destroy, { user_id: '10', id: @song.id })
+
+        reply = JSON.parse(response.body, symbolize_names: true)
+
+        expect(reply[:errors]).to be_eql("Not authenticated")
+        expect(response.status).to eq(401)
+      end
+    end
+  end
 end
